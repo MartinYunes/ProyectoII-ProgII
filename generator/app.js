27a2df8271b07e9c72ce980450ad2fc8d4df6ad8
MateoSilveri <msilveri@udesa.edu.ar>
2023-04-04 13:19:48 -0300

    nuevas rutas

--- generator/app.js
+++ generator/app.js
@@ -21,7 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/nashei', usersRouter)
+app.use('/login', usersRouter);
+app.use('/register', usersRouter);
+app.use('search-results', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
