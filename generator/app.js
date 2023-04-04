6382fd372a7ad3744873aa6ed8a107a00c711783
MartinYunes <myunesmarangoni@udesa.edu.ar>
2023-04-04 18:48:15 -0300

    cambios app.js

--- generator/app.js
+++ generator/app.js
@@ -6,6 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
+var productRouter = require('./routes/product')
+var profileRouter = require('./routes/profile')
+var searchresultsRouter = require('./routes/search-results')
+
 
 var app = express();
 
@@ -21,9 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', usersRouter);
-app.use('/register', usersRouter);
-app.use('/search-results', usersRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use('/product', productRouter);
+app.use('/profile', profileRouter);
+app.use('/search-results', searchresultsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
