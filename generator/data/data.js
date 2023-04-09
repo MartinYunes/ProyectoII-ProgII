ecfabc888e5016b73fa552d8802b41c59dfe010b
MateoSilveri <msilveri@udesa.edu.ar>
2023-04-09 19:55:58 -0300

    estructura data

--- generator/data/data.js
+++ generator/data/data.js
@@ -1,17 +1,54 @@
 let data = {
     usuario:{
+        userName: '',
+        pw: '',
+        email: '',
+        dni: '',
+        fotoPerfil: '',
+
 
     },
     productos: [{
+        modeloAuto: '',
+        imagen: '',
+        descripcion: '',
+        fechaPub: '',
+        id: 1
 
     },
     {
-
+        modeloAuto: '',
+        imagen: '',
+        descripcion: '',
+        fechaPub: '',
+        id: 2
     },
+    {
+        modeloAuto: '',
+        imagen: '',
+        descripcion: '',
+        fechaPub: '',
+        id: 3
+    },
+    {
+        modeloAuto: '',
+        imagen: '',
+        descripcion: '',
+        fechaPub: '',
+        id: 4
+    }
 
     ],
     comentarios: [ {
+        userName: '',
+        comentario: '',
+        fotoPerfil: '',
 
+    },
+    {
+        userName: '',
+        comentario: '',
+        fotoPerfil: '',
     },
 
     ]
