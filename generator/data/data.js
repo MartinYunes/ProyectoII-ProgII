56c47e9e7c20b85173ccf226f85e088884a75cc8
MartinYunes <myunesmarangoni@udesa.edu.ar>
2023-04-10 09:27:44 -0300

    cambios en data

--- generator/data/data.js
+++ generator/data/data.js
@@ -9,9 +9,9 @@ let data = {
 
     },
     productos: [{
-        modeloAuto: '',
+        modeloAuto: 'Tesla Model S',
         imagen: '',
-        descripcion: '',
+        descripcion: 'El Tesla Model S es un sedán eléctrico de lujo que ofrece un rendimiento excepcional y una autonomía impresionante. Es uno de los vehículos eléctricos más populares del mercado.',
         fechaPub: '',
         id: 1,
         comentarios: [ {
@@ -23,9 +23,9 @@ let data = {
 
     },
     {
-        modeloAuto: '',
+        modeloAuto: 'Ford Mustang',
         imagen: '',
-        descripcion: '',
+        descripcion: 'El Ford Mustang es un icónico auto deportivo estadounidense que ofrece un rendimiento emocionante, un estilo clásico y una amplia gama de opciones de personalización.',
         fechaPub: '',
         id: 2,
         comentarios: [ {
@@ -36,9 +36,9 @@ let data = {
         }]
     },
     {
-        modeloAuto: '',
+        modeloAuto: 'Lambroghini Aventador',
         imagen: '',
-        descripcion: '',
+        descripcion: 'El Lamborghini Aventador es un súper deportivo italiano que ofrece un rendimiento de clase mundial, un diseño futurista y un precio exorbitante.',
         fechaPub: '',
         id: 3,
         comentarios: [ {
@@ -49,9 +49,9 @@ let data = {
         }]
     },
     {
-        modeloAuto: '',
+        modeloAuto: 'Chevrolet Camaro',
         imagen: '',
-        descripcion: '',
+        descripcion: 'El Chevrolet Camaro es un auto deportivo americano que ofrece una experiencia de conducción emocionante, un estilo agresivo y un precio accesible.',
         fechaPub: '',
         id: 4,
         comentarios: [ {
@@ -62,9 +62,9 @@ let data = {
         }]
     },
     {
-        modeloAuto: '',
+        modeloAuto: 'BMW M5',
         imagen: '',
-        descripcion: '',
+        descripcion: 'El BMW M5 es un sedán deportivo de lujo que ofrece un rendimiento excepcional, una tecnología avanzada y un estilo elegante',
         fechaPub: '',
         id: 5,
         comentarios: [ {
@@ -75,9 +75,9 @@ let data = {
         }]
     },
     {
-        modeloAuto: '',
+        modeloAuto: 'Audi R8',
         imagen: '',
-        descripcion: '',
+        descripcion: 'El Audi R8 es un súper deportivo alemán que ofrece un rendimiento impresionante, un diseño aerodinámico y una tecnología de vanguardia.',
         fechaPub: '',
         id: 6,
         comentarios: [ {
@@ -88,9 +88,9 @@ let data = {
         }]
     },
     {
-        modeloAuto: '',
+        modeloAuto: 'Porsche 911',
         imagen: '',
-        descripcion: '',
+        descripcion: 'El Porsche 911 es un auto deportivo alemán que ofrece un rendimiento de alto nivel, un diseño icónico y una experiencia de conducción emocionante.',
         fechaPub: '',
         id: 7,
         comentarios: [ {
@@ -101,9 +101,9 @@ let data = {
         }]
     },
     {
-        modeloAuto: '',
+        modeloAuto: 'Mercedez-Benz AMG GT ',
         imagen: '',
-        descripcion: '',
+        descripcion: ' El Mercedes-Benz AMG GT es un súper deportivo de lujo que ofrece un rendimiento de clase mundial, un diseño elegante y una tecnología avanzada.',
         fechaPub: '',
         id: 8,
         comentarios: [ {
@@ -114,9 +114,9 @@ let data = {
         }]
     },
     {
-        modeloAuto: '',
+        modeloAuto: 'Dodge Challenger',
         imagen: '',
-        descripcion: '',
+        descripcion: 'El Dodge Challenger es un auto deportivo americano que ofrece un estilo retro, una experiencia de conducción emocionante y una amplia variedad de opciones de motor.',
         fechaPub: '',
         id: 9,
         comentarios: [ {
@@ -127,9 +127,9 @@ let data = {
         }]
     },
     {
-        modeloAuto: '',
+        modeloAuto: 'Toyota Supra',
         imagen: '',
-        descripcion: '',
+        descripcion: 'El Toyota Supra es un deportivo japonés que ofrece un rendimiento emocionante, un diseño elegante y una historia icónica en el mundo de los autos deportivos.',
         fechaPub: '',
         id: 10,
         comentarios: [ {
