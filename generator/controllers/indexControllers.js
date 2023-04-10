07f9a2e7f382670511adae4a61b6b736de027ba8
MateoSilveri <msilveri@udesa.edu.ar>
2023-04-09 22:04:14 -0300

    header

--- generator/controllers/indexControllers.js
+++ generator/controllers/indexControllers.js
@@ -3,7 +3,8 @@ const data = require('../data/data')
 const controller = {
     index: function(req,res){
         res.render('index', {
-            productos: data.productos
+            productos: data.productos,
+            usuarioLogueado: false,
         })
     }
 }
