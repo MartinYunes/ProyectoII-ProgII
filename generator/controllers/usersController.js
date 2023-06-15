dfabb9c99a23f037750e4c2cf681b961d5b54920
MateoSilveri <msilveri@udesa.edu.ar>
2023-06-14 22:42:12 -0300

    datos del registro hacia sql

--- generator/controllers/usersController.js
+++ generator/controllers/usersController.js
@@ -9,9 +9,28 @@ var userController = {
 
     store: function(req,res) {
         let info = req.body /* trae toda la info del formulafrio */
-        console.log(info);
         
-        return res.redirect('/users/login')
+        let userStore = {
+            usuario: info.usuario,
+            email: info.email,
+            contraseña: info.contraseña,
+            foto_perfil: info.foto_perfil,
+            fecha_nacimiento: info.fecha_nacimiento,
+            DNI: info.DNI
+            
+
+        }
+        
+        usuarios.create(userStore)
+        .then(function (result) {
+            return res.redirect('/users/login');
+        })
+
+        .catch(function (error) {
+            console.log(error);
+        })
+
+        
     },
 
 
